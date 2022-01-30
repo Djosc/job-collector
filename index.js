07d1fc8e53adc097dde58064f18e8c8d4f4bea3b
Djosc <davidjoscaldwell@gmail.com>
2022-01-29 21:44:39 -0500

    Empty scrape.js and fix indeed pagination issue

--- index.js
+++ index.js
@@ -10,21 +10,21 @@ import express from 'express';
 const PORT = process.env.PORT || 3000;
 
 // Will eventually add an input for users(me) to use custom query parameters
-var URL = 'https://www.indeed.com/jobs?q=Web%20Developer&l=Dayton%2C%20OH';
+var indeedURL = 'https://www.indeed.com/jobs?q=Web%20Developer&l=Dayton%2C%20OH';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// This retrieves the url of the full job description, then sends it to the scraping function.
-app.get('/jobs', async (req, res) => {
+app.get('/indeedJobs', async (req, res) => {
 	const promises = [];
 	const jobs = [];
 
+	// indeed pages increment by 10. Store a promise with an axios call for each page
 	for (let i = 0; i <= 20; i += 10) {
 		promises.push(
-			axios({ method: 'get', url: `${URL}&start${i}` })
+			axios({ method: 'get', url: `${indeedURL}&start=${i}` })
 				.then((response) => {
 					const htmlData = response.data;
 					const $ = cheerio.load(htmlData);
@@ -55,10 +55,15 @@ app.get('/jobs', async (req, res) => {
 		);
 	}
 
+	console.log(promises);
+
+	// Run the axios calls from the promises array
 	Promise.all(promises)
 		.then(() => {
 			console.log(jobs);
 			console.log(jobs.length);
+
+			// Allow jobs array to populate, then return
 			setTimeout(() => res.status(200).json(jobs), 5000);
 		})
 		.catch((err) => console.error(err));
--- src/scrape.js
+++ src/scrape.js
@@ -3,48 +3,4 @@ import cheerio from 'cheerio';
 
 export const jobData = [];
 
-export const scrapeAndPush = (url) => {
-	url = 'https://www.indeed.com' + url;
-	axios(url).then((res) => {
-		const htmlData = res.data;
-		const $ = cheerio.load(htmlData);
-		// console.log(url);
-
-		$('.jobsearch-ViewJobLayout-jobDisplay', htmlData).each((index, element) => {
-			const title = $(element)
-				.find('.jobsearch-JobInfoHeader-title-container > h1')
-				.text();
-
-			const company = $(element)
-				.find('.jobsearch-CompanyInfoContainer')
-				.find('a')
-				.first()
-				.text();
-
-			const location = $(element).find('.jobsearch-jobLocationHeader-location').text();
-
-			const qualifications = $(element)
-				.find('#jobDescriptionText')
-				.find(
-					'p:contains("qualifications"), p:contains("minimum"), p:contains("requirements")'
-				)
-				.text()
-				.trim();
-			// const description = $(element)
-			// 	.find('#jobDescriptionText')
-			// 	.find('p:contains("required")')
-			// 	.text()
-			// 	.trim();
-
-			if (company !== 'Revature') {
-				jobData.push({
-					title,
-					company,
-					location,
-					qualifications,
-					// description,
-				});
-			}
-		});
-	});
-};
+export const scrapeAndPush = (url) => {};
