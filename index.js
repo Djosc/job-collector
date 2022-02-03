ee8a36baa71376d411f9ef296eb9d5b7205ba45f
Djosc <davidjoscaldwell@gmail.com>
2022-02-02 19:38:09 -0500

    Refactor scrape function to take in query params

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import cheerio from 'cheerio';
 import express from 'express';
 import puppeteer from 'puppeteer';
 
-// import { scrapeAndPush } from './src/scrape.js';
+import { scrapeIndeed } from './src/scrape.js';
 // import { jobData } from './src/scrape.js';
 
 const PORT = process.env.PORT || 3000;
@@ -20,55 +20,18 @@ app.use(express.urlencoded({ extended: true }));
 
 // Get Indeed Job Data and Links
 app.get('/indeedJobs', async (req, res) => {
-	const promises = [];
-	const jobs = [];
+	const queryParams = {
+		jobQuery: 'Web Developer',
+		cityQuery: 'Dayton, OH',
+		numberOfPages: 10,
+	};
 
-	// indeed pages increment by 10. Store a promise with an axios call for each page
-	for (let i = 0; i <= 20; i += 10) {
-		promises.push(
-			axios({ method: 'get', url: `${indeedURL}&start=${i}` })
-				.then((response) => {
-					const htmlData = response.data;
-					const $ = cheerio.load(htmlData);
-
-					$('#mosaic-provider-jobcards > a', htmlData).each((index, element) => {
-						const title = $(element).find('.jobTitle > span').text().trim();
-						const company = $(element).find('.companyName').text().trim();
-						const location = $(element).find('.companyLocation').text().trim();
-						const pay = $(element).find('.attribute_snippet').text().trim();
-						const description = $(element).find('.job-snippet > ul').text().trim();
-						const linkToFullJob = $(element).attr('href');
-
-						// const encodedLink = encodeURIComponent(linkToFullJob);
-
-						if (company !== 'Revature') {
-							jobs.push({
-								title: title,
-								company: company,
-								location: location,
-								pay: pay,
-								description: description,
-								linkToFullJob: 'https://indeed.com' + linkToFullJob,
-							});
-						}
-					});
-				})
-				.catch((err) => console.error(err))
-		);
-	}
-
-	console.log(promises);
-
-	// Run the axios calls from the promises array
-	Promise.all(promises)
-		.then(() => {
-			console.log(jobs);
-			console.log(jobs.length);
-
-			// Allow jobs array to populate, then return
-			setTimeout(() => res.status(200).json(jobs), 5000);
+	scrapeIndeed(queryParams)
+		.then((data) => {
+			console.log(data.length);
+			setTimeout(() => res.status(200).json(data), 5000);
 		})
-		.catch((err) => console.error(err));
+		.catch((err) => res.status(500));
 });
 
 //Get GlassDoor Job Data and Links
--- src/scrape.js
+++ src/scrape.js
@@ -1,6 +1,55 @@
 import axios from 'axios';
 import cheerio from 'cheerio';
 
-export const jobData = [];
+export const scrapeIndeed = async (queryParams) => {
+	const indeedURL = 'https://www.indeed.com/';
+	const { jobQuery, cityQuery, numberOfPages } = queryParams;
 
-export const scrapeAndPush = (url) => {};
+	// Combine the query strings and encode the Url
+	let queryURL = indeedURL + 'jobs';
+	queryURL += '?q=' + jobQuery;
+	queryURL += '&l=' + cityQuery;
+
+	const qURLEncoded = encodeURI(queryURL);
+
+	const promises = [];
+	const jobs = [];
+
+	// indeed pages increment by 10. Store a promise with an axios call for each page
+	for (let i = 0; i <= numberOfPages; i += 10) {
+		promises.push(
+			axios({ method: 'get', url: `${qURLEncoded}&start=${i}` })
+				.then((response) => {
+					const htmlData = response.data;
+					const $ = cheerio.load(htmlData);
+
+					$('#mosaic-provider-jobcards > a', htmlData).each((index, element) => {
+						const title = $(element).find('.jobTitle > span').text().trim();
+						const company = $(element).find('.companyName').text().trim();
+						const location = $(element).find('.companyLocation').text().trim();
+						const tags = $(element).find('.attribute_snippet').text().trim();
+						const description = $(element).find('.job-snippet > ul').text().trim();
+						const linkToFullJob = $(element).attr('href');
+
+						if (company !== 'Revature') {
+							jobs.push({
+								title: title,
+								company: company,
+								location: location,
+								tags: tags,
+								description: description,
+								linkToFullJob: 'https://indeed.com' + linkToFullJob,
+							});
+						}
+					});
+					return jobs;
+				})
+				.catch((err) => console.error(err))
+		);
+	}
+
+	// Run the axios calls from the promises array.
+	// This resolves to a single promise containing the job data, which is returned to the endpoint,
+	// Then the data is sent in a json response.
+	return Promise.all(promises);
+};
